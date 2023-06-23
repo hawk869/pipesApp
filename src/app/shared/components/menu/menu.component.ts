35e99551a58e071fa38a0c23c5c40e5b879adac8
Daniel Esteban <decepeda@misena.edu.co>
2023-06-23 17:52:29 -0500

    routerLink navegando desde el menuBar

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -20,15 +20,18 @@ export class MenuComponent {
                 items: [
                     {
                         label: 'Textos y Fechas',
-                        icon: 'pi pi-align-center'
+                        icon: 'pi pi-align-center',
+                        routerLink: '/'
                     },
                     {
                         label: 'Numeros',
-                        icon: 'pi pi-dollar'
+                        icon: 'pi pi-dollar',
+                        routerLink: 'numbers'
                     },
                     {
                         label: 'No comunes',
-                        icon: 'pi pi-globe'
+                        icon: 'pi pi-globe',
+                        routerLink: 'uncommon'
                     }
                 ]
             },
