6e9116a017fc32d2f14077124286150d603d585d
Daniel Esteban <decepeda@misena.edu.co>
2023-06-24 12:54:16 -0500

    i18nSelect Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  public name: string = 'Daniel';
+  public gender: 'male'|'female' = 'male';
+
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
 }
