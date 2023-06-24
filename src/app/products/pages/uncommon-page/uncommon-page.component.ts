70a4aeb35c0014a6b9fcae0feacd08bf2e6fbf1f
Daniel Esteban <decepeda@misena.edu.co>
2023-06-24 13:08:02 -0500

    i18nSelect Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,17 +7,27 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18nSelect Pipe
   public name: string = 'Daniel';
   public gender: 'male'|'female' = 'male';
-
   public invitationMap = {
     'male': 'invitarlo',
     'female': 'invitarla'
   }
-
   changeClient(): void {
     this.name = 'Melissa';
     this.gender = 'female';
+  }
+
+//   i18nPlural Pipe
+  public clients: string[] = ['Maria','Daniel','Juan','Fernando','Diana','Valentina',];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.',
+  }
+  deleteClient(): void {
+    this.clients.shift();
   }
 
 }
