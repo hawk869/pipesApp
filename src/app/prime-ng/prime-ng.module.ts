d98b58035eadc71d28db60b30ada7ad7816c02da
Daniel Esteban <decepeda@misena.edu.co>
2023-06-25 19:30:55 -0500

    agregando toolbar de primeng a nuestro order.component.ts

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MenuModule } from 'primeng/menu';
+// import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
 import {ButtonModule} from "primeng/button";
 import {CardModule} from "primeng/card";
 import {FieldsetModule} from "primeng/fieldset";
 import {PanelModule} from "primeng/panel";
+import {ToolbarModule} from "primeng/toolbar";
 
 @NgModule({
   exports: [
@@ -13,7 +14,8 @@ import {PanelModule} from "primeng/panel";
     CardModule,
     FieldsetModule,
     MenubarModule,
-    PanelModule
+    PanelModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'products-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent {
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Update',
+        icon: 'pi pi-refresh'
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-times'
+      },
+      {
+        label: 'Angular',
+        icon: 'pi pi-external-link',
+        url: 'http://angular.io'
+      },
+      {
+        label: 'Router',
+        icon: 'pi pi-upload',
+        routerLink: '/fileupload'
+      }
+    ];
+  }
 
 }
