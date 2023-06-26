7b4a06d1ff4e47e60c5decfd96767ad31a740d70
Daniel Esteban <decepeda@misena.edu.co>
2023-06-25 19:37:19 -0500

    se agrega argumento dinamico al pipe personalizado

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'products-order',
@@ -7,29 +6,10 @@ import {MenuItem} from "primeng/api";
 })
 export class OrderComponent {
 
-  items: MenuItem[] | undefined;
+  public isUpperCase: boolean = false;
 
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Update',
-        icon: 'pi pi-refresh'
-      },
-      {
-        label: 'Delete',
-        icon: 'pi pi-times'
-      },
-      {
-        label: 'Angular',
-        icon: 'pi pi-external-link',
-        url: 'http://angular.io'
-      },
-      {
-        label: 'Router',
-        icon: 'pi pi-upload',
-        routerLink: '/fileupload'
-      }
-    ];
+  toggleUpperCase(): void {
+    this.isUpperCase = !this.isUpperCase;
   }
 
 }
